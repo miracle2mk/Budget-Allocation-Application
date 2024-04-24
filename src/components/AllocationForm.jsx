0f3ca014fb90f4944c188e2f04bcd4dbf3b3bdb7
Mikiyas Bayew <165364328+miracle2mk@users.noreply.github.com>
2024-04-24 10:31:02 +0300

    Update AllocationForm.jsx

--- src/components/AllocationForm.jsx
+++ src/components/AllocationForm.jsx
@@ -36,38 +36,6 @@ const AllocationForm = (props) => {
 
   return (
     <div>
-      <div className="row">
-        <div className="input-group mb-3" style={{ marginLeft: '2rem' }}>
-          <div className="input-group-prepend">
-            <label className="input-group-text" htmlFor="inputGroupSelect01">
-              Department
-            </label>
-          </div>
-          <select
-            className="custom-select"
-            id="inputGroupSelect01"
-            onChange={(event) => setName(event.target.value)}
-          >
-            <option defaultValue>Choose...</option>
-            <option value="Marketing" name="marketing">
-              Marketing
-            </option>
-            <option value="Sales" name="sales">
-              Sales
-            </option>
-            <option value="Finance" name="finance">
-              Finance
-            </option>
-            <option value="HR" name="HR">
-              HR
-            </option>
-            <option value="IT" name="it">
-              IT
-            </option>
-            <option value="Admin" name="admin">
-              Admin
-            </option>
-          </select>
 <div className="row">
         <div className="input-group mb-3" style={{ marginLeft: '2rem' }}>
           <div className="input-group-prepend">
